3ce4a1aa25116ea6cf1729173ae94278562a0454
Christofer Nyström <nystromchristofer@gmail.com>
2026-06-22 23:43:13 +0200

    adds open api

--- MovieApi/Program.cs
+++ MovieApi/Program.cs
@@ -7,13 +7,18 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddDbContext<MovieApiContext>(options =>
     options.UseSqlServer(builder.Configuration.GetConnectionString("MovieApiContext") ?? throw new InvalidOperationException("Connection string 'MovieApiContext' not found.")));
 
-
 // Add services to the container.
 
 builder.Services.AddControllers();
-
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddOpenApi();
 
 var app = builder.Build();
+
+if (app.Environment.IsDevelopment())
+{
+    app.MapOpenApi();
+}
 
 using (var scope = app.Services.CreateScope())
 {
@@ -21,14 +26,8 @@ using (var scope = app.Services.CreateScope())
     await DbSeeder.SeedAsync(context);
 }
 
-
-
 // Configure the HTTP request pipeline.
-
 app.UseHttpsRedirection();
-
 app.UseAuthorization();
-
 app.MapControllers();
-
 app.Run();
