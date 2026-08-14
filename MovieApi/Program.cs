4bcd2a8d576d16b7239e9c66a53e7d8439944e04
Christofer Nyström <nystromchristofer@gmail.com>
2026-08-14 10:03:02 +0200

    fixes so docker builds properly

--- MovieApi/Program.cs
+++ MovieApi/Program.cs
@@ -37,6 +37,8 @@ if (app.Environment.IsDevelopment())
 using (var scope = app.Services.CreateScope())
 {
     var context = scope.ServiceProvider.GetRequiredService<MovieApiContext>();
+
+    await context.Database.MigrateAsync();
     await DbSeeder.SeedAsync(context);
 }
 
